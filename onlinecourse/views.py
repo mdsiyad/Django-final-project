062cd63087adee89f34f718d9a731fcc6537db11
mdsiyad <engsiyad2020@gmail.com>
2023-03-11 08:25:47 -0500

    update view

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -121,7 +121,8 @@ def enroll(request, course_id):
          # Collect the selected choices from exam form
          # Add each selected choice object to the submission object
          # Redirect to show_exam_result with the submission id
-#def submit(request, course_id):
+def submit(request, course_id):
+    
 
 
 # <HINT> A example method to collect the selected choices from the exam form from the request object
